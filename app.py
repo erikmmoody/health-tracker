37bc1e2516e7bf9b805b02743b76a5243f072acc
Erik Moody <emoody@gaggle.net>
2023-01-24 20:15:21 -0600

    create pipeline stack

--- app.py
+++ app.py
@@ -7,22 +7,27 @@ from health_tracker.health_tracker_stack import HealthTrackerStack
 
 
 app = cdk.App()
-HealthTrackerStack(app, "HealthTrackerStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+HealthTrackerStack(
+    app,
+    "HealthTrackerStack",
+    env=cdk.Environment(account="504720764336", region="us-east-1")
+)
+# HealthTrackerStack(app, "HealthTrackerStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
+#
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
+#
+#     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+#
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
+#
+#     #env=cdk.Environment(account='123456789012', region='us-east-1'),
+#
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#     )
 
 app.synth()
